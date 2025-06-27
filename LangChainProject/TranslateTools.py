d8cbb0da978b70ed6287d5dae4bba2b6c7dd531c
chanhokwong <chanhokwong718@gmail.com>
2025-06-27 15:43:37 +0800

    Update TranslateTools.py

--- LangChainProject/TranslateTools.py
+++ LangChainProject/TranslateTools.py
@@ -1,4 +1,3 @@
-# 教如何調用ai模型
 import os
 
 from langchain_core.messages import SystemMessage, HumanMessage
@@ -30,28 +29,32 @@ parser = StrOutputParser()  # 文本解析器
 
 # 定義提示模板 (輸入給大模型的模板)
 prompt_template = ChatPromptTemplate.from_messages([
-    ('system', '請將下面的內容翻譯成{language}'),
-    ('user', '{text}'),
+    ('system', '請將下面的內容翻譯成{language}'),  # 設置變量language 用以接收用戶設置的翻譯語言
+    ('user', '{text}'),  # 設置變量text 用以接收用戶傳入的問題
 ])
 
-# 4. 得到鏈
-chain = prompt_template | model | parser
+# 4. 設置鏈 可以理解為一個工作流workflow
+# 首先 通過提示模板獲得用戶的問題 然後將模板返回到大模型中 隨後大模型將作出回答 而我們將回答傳到解析器中 僅獲得字符串格式的回答
+chain = prompt_template | model | parser  
 
-# 5. 直接使用chain來調用
+# 5. 測試流程是否正常 傳參是否順利
+# 直接使用chain來調用
 # print(chain.invoke({'language': 'English', 'text': '我下午還有一節課,不能去打球了。'}))
 
-# 要把我們的程序部署成服務
-# 創建fastAPI的應用
-# title是你應用的名稱 version是你服務的版本(可自定義)
-app = FastAPI(title="我的Langchain服務", version="v1.0", description="使用Langchain翻譯任何語句的服務器")  # 這步是創建了fastapi的應用
+# 6. 服務部署 fastAPI
+# 要把我們的程序部署成服務 需創建fastAPI的應用
+# 創建變量app 用以儲存FastAPI對象
+app = FastAPI(title="我的Langchain服務",  # 你的應用名稱(可自定義)
+              version="v1.0",  # 你的應用版本(可自定義)
+              description="使用Langchain翻譯任何語句的服務器")  # 描述你的應用(可自定義)
 
 # 添加路由
 add_routes(
-    app,
-    chain,
-    path="/chain"  # 路由可以隨便改
+    app,  # FastAPI信息
+    chain,  # 鏈 要執行的工作流
+    path="/chain"  # 路由(可以隨便改)
 )
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8002)
+    uvicorn.run(app, host="localhost", port=8002)  # 運行服務器 host為127.0.0.1 所以url為127.0.0.1:8002/chain/invoke
